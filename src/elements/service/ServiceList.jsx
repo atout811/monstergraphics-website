6b9c3926dd6b02bc297ac4fea4ad24515fb6c599
Mahmoud Hammad <hodaman2012@gmail.com>
2022-11-29 22:22:55 +0200

    our services icon section

--- src/elements/service/ServiceList.jsx
+++ src/elements/service/ServiceList.jsx
@@ -1,60 +1,85 @@
-import React ,{ Component }from "react";
-import { FiCast , FiLayers , FiSmartphone , FiMonitor } from "react-icons/fi";
+import React, { Component } from "react";
+import {
+  FiNavigation,
+  FiTrendingUp,
+  FiShoppingCart,
+  FiSearch,
+  FiSmartphone,
+  FiMonitor,
+} from "react-icons/fi";
 
 const ServiceList = [
-    {
-        icon: <FiMonitor />,
-        title: 'Website Development',
-        description: ''
-    },
-    { 
-        icon: <FiSmartphone />,
-        title: 'Mobile App Development',
-        description: ''
-    },
-    // {
-    //     icon: <FiCast />,
-    //     title: 'Business Stratagy',
-    //     description: 'I throw myself down among the tall grass by the stream as I lie close to the earth.'
-    // },
-    
-    // {
-    //     icon: <FiUsers />,
-    //     title: 'Marketing & Reporting',
-    //     description: 'I throw myself down among the tall grass by the stream as I lie close to the earth.'
-    // },
-   
-    
-]
+  {
+    icon: <FiMonitor />,
+    title: "Website Development",
+    description: "Single page applications using latest technologies available",
+  },
+  {
+    icon: <FiSmartphone />,
+    title: "Mobile App Development",
+    description: "Cross platform applications for Android and IOS",
+  },
+  {
+    icon: (
+      <img
+        width={42}
+        height={42}
+        src="/assets/icons/digital-marketing.png"
+        alt="Social media marketing"
+      />
+    ),
+    title: "Social media marketing,",
+    description: "Build brands, increase sales, and drive website traffic",
+  },
+  {
+    icon: <FiSearch />,
+    title: "Search engine optimization",
+    description: "Rank higher in search engines to receive more traffic",
+  },
+  {
+    icon: <FiTrendingUp />,
+    title: "Search Engine Marketing ",
+    description:
+      "increase visibility in search engine primarily through ads",
+  },
 
+  // {
+  //     icon: <FiCast />,
+  //     title: 'Business Stratagy',
+  //     description: 'I throw myself down among the tall grass by the stream as I lie close to the earth.'
+  // },
 
-class ServiceThree extends Component{
-    render(){
-        const {column } = this.props;
-        const ServiceContent = ServiceList.slice(0 , this.props.item);
-        
-        return(
-            <React.Fragment>
-                <div className="row service-main-wrapper">
-                    {ServiceContent.map( (val , i) => (
-                        <div className={`${column}`} key={i}>
-                            {/* <a href="/service-details"> */}
+  // {
+  //     icon: <FiUsers />,
+  //     title: 'Marketing & Reporting',
+  //     description: 'I throw myself down among the tall grass by the stream as I lie close to the earth.'
+  // },
+];
 
-                                <div className="service service__style--2 text-left bg-gray">
-                                    <div className="icon">
-                                        {val.icon}
-                                    </div>
-                                    <div className="content">
-                                        <h3 className="title">{val.title}</h3>
-                                        <p>{val.description}</p>
-                                    </div>
-                                </div>
+class ServiceThree extends Component {
+  render() {
+    const { column } = this.props;
+    const ServiceContent = ServiceList.slice(0, this.props.item);
 
-                        </div>
-                    ))}
+    return (
+      <React.Fragment>
+        <div className="row service-main-wrapper">
+          {ServiceContent.map((val, i) => (
+            <div className={`${column}`} key={i}>
+              {/* <a href="/service-details"> */}
+
+              <div className="service service__style--2 text-left bg-gray">
+                <div className="icon">{val.icon}</div>
+                <div className="content">
+                  <h3 className="title">{val.title}</h3>
+                  <p>{val.description}</p>
                 </div>
-            </React.Fragment>
-        )
-    }
+              </div>
+            </div>
+          ))}
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 export default ServiceThree;
