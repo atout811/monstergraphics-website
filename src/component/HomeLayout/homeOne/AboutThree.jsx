0af790dbe5a3c88ae914b8a1887d72a93b8a3ade
Mahmoud Hammad <hodaman2012@gmail.com>
2022-11-29 22:54:17 +0200

    About Done

--- src/component/HomeLayout/homeOne/AboutThree.jsx
+++ src/component/HomeLayout/homeOne/AboutThree.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { FiSend, FiCheck } from "react-icons/fi";
+import {FiUser,FiTarget,FiStar,FiSquare,FiSmile, FiSend, FiCheck } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
 class AboutThree extends Component {
@@ -8,14 +8,15 @@ class AboutThree extends Component {
       description = "";
     var namesItemOne = [
       "Established in 2020",
-      "multidisciplinary services company catering to a global clientele with end-to-end digital enablement capabilities.",
-      //   "Here’s a quick snapshot of our journey.",
-      "Satisfied customers in 10+ countries 22 clients across the globe",
-      "Successful delivery of more than 30 projects including application development and digital marketing services",
-      "We are providing digital marketing solutions. Our 360-strong organization comprises ERP, AI, Blockchain, OTT, IoT, AR/VR, DevOps, DPP, UI/UX, and Digital Marketing CoEs.",
+      "Satisfied customers in +10 countries 22 clients across the globe",
+      "Successful delivery of +30 projects",
+      "Multidisciplinary services company catering to a global clientele ",
+      "Provide end-to-end digital enablement capabilities.",
+      "We are providing digital marketing solutions.",
       "We take pride in enabling our clients to drive sales and organizational growth.",
-      "MonstersGraphics has always been at the leading edge of driving innovation in the offshore development industry.",
-      "Our clients appreciate the value we bring to the table with reduced time to market, high-quality output, and technological innovation at competitive costs without compromising on quality.",
+      "Our 360-strong organization comprises ERP, AI, OTT, AR/VR, DevOps, DPP, and UI/UX",
+      // "MonstersGraphics has always been at the leading edge of driving innovation in the offshore development industry.",
+      // "Our clients appreciate the value we bring to the table with reduced time to market, high-quality output, and technological innovation at competitive costs without compromising on quality.",
     ];
 
     return (
@@ -23,21 +24,19 @@ class AboutThree extends Component {
         <div className="about-wrapper">
           <div className="container">
             <div className="row row--35 align-items-center">
-              <div className="col-lg-5 col-md-12">
+              <div className="col-lg-4 col-md-12">
                 <div className="thumbnail">
                   <img
-                    className="w-100"
+                    // width="90%"
                     src="/assets/images/about/about-1.png"
                     alt="About Images"
                   />
                 </div>
               </div>
-              <div className="col-lg-7 col-md-12">
+              <div className="col-lg-8 col-md-12">
                 <div className="about-inner inner">
                   <div className="section-title">
-                    <div className="icon">
-                      <FiSend />
-                    </div>
+                     
                     <h2 className="title">{title}</h2>
                     <h2 className="describe">{description}</h2>
                     <ul className="list-style--1">
